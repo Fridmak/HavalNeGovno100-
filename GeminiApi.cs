94998368ea6451e4c9255100b12c55ac7cac52e5
Fridmak <androlimp22@gmail.com>
2025-03-28 18:21:13 +0500

    initial commit

---
+++ GeminiApi.cs
@@ -0,0 +1,24 @@
+﻿using AutoGen.Core;
+using AutoGen.Gemini;
+
+namespace HavalNeGovno;
+
+public class GeminiApi
+{
+    private static string apiKey;
+    private MiddlewareStreamingAgent<GeminiChatAgent> agent;
+    
+    GeminiApi()
+    {
+        apiKey = Environment.GetEnvironmentVariable("GOOGLE_GEMINI_API_KEY");
+
+        agent = new GeminiChatAgent(
+                name: "gemini",
+                model: "gemini-1.5-flash-001",
+                apiKey: apiKey,
+                systemMessage: "You are a helpful C# engineer, put your code between ```csharp and ```, don't explain the code")
+            .RegisterMessageConnector()
+            .RegisterPrintMessage();
+
+    }
+}
